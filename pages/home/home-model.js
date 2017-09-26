42a6323464609046e9ca224fe8b2e361992747f5
nightwaking <1105468795@qq.com>
2017-09-26 20:59:04 +0800

    定义banner的model模型获取服务器端数据

--- pages/home/home-model.js
+++ pages/home/home-model.js
@@ -5,12 +5,15 @@
  * 使用ES6 class关键字定义类
  */
 class Home{
+  constructor(){
+
+  }
   /**
    * 获取banner数据
    */
-  getBannerData(){
+  getBannerData(id){
     wx.request({
-      url: 'http://www.wxservlet.com/api/v1/banner/#id',
+      url: 'http://www.wxservlet.com/api/v1/banner/' + id,
       method: 'GET',
       // 定义回调函数, 异步
       success:function(res){
@@ -19,3 +22,5 @@ class Home{
     })
   }
 }
+
+export {Home};
