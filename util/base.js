c29a377400d93ffe4085de3ab97ab23c5113e43d
nightwaking <1105468795@qq.com>
2017-09-29 21:39:19 +0800

    定义基类，创建request方法，封装http请求

---
+++ util/base.js
@@ -0,0 +1,41 @@
+import {Config} from '../utils/config.js';
+/**
+ * 请求基类
+ */
+class Base{
+  constructor(){
+    this.baseRequestUrl = Config.restUrl;
+  }
+
+  /**
+   * 定义request方法,封装http请求
+   */
+  request(params, ){
+    var url = this.baseRequestUrl + params.url;
+    // 传入的params.type为空，设置为GET
+    if(!params.type){
+      params.type = 'GET'
+    }
+    wx.request({
+      // 请求api的地址
+      url: url,
+      // post请求的body参数
+      data: params.data,
+      method: params.type,
+      header: {
+        'content-type': 'application/json',
+        'token': wx.getStorageSync('token')
+      },
+      success:function(res){
+        // if (params.sCallBack){
+        //   // 回调函数
+        //   params.sCallBack(res);
+        // }
+        params.sCallBack && params.sCallBack(res);
+      },
+      fail:function(err){
+
+      }
+    })
+  }
+}
