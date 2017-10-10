48ea6dc58348d6471a17da192697e7998a071914
nightwaking <1105468795@qq.com>
2017-10-10 21:44:35 +0800

    主题页面的编写，跳转页面携带主题id

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,17 +1,33 @@
 // theme.js
+import { Theme } from 'theme-model.js';
+var theme = new Theme();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var id = options.id;
+    var name = options.name;
+    this.data.id = id;
+    this.data.name = name;
+    this._loadData();
+  },
   
+  _loadData:function(){
+    theme.getProductsData(this.data.id, (res)=>{
+      console.log(res);
+      this.setData({
+        'themeInfo': res
+      });
+    });
   }
 })
