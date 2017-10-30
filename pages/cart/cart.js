b2968063424bbc9d66153a6ef5c5c7527017ec20
nightwaking <1105468795@qq.com>
2017-10-30 20:51:27 +0800

    购物车单选按钮

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -24,12 +24,13 @@ Page({
    */
   onShow: function () {
     var cartData = cart.getCartDataFormLocal();
-    var countsInfo = cart.getCartTotalCounts(true);
-
+    var cal = this._calcTotalAccountAndCounts(cartData);
+    
     this.setData({
-      'selectedCounts': countsInfo,
+      'selectedCounts': cal.selectedCounts,
       'cartData': cartData,
-
+      'selectedTypeCount': cal.selectTypeCounts,
+      'account': cal.account
     });
   },
 
@@ -47,7 +48,7 @@ Page({
     // 避免浮点数运算误差
     let multiple = 100;
     for (let i = 0; i < len; i++){
-      if (data[i].selectedStatus){
+      if (data[i].selectStatus){
         account += data[i].counts * multiple * Number(data[i].price) * multiple;
         selectedCounts += data[i].counts;
         selectedTypeCounts ++;
@@ -59,5 +60,50 @@ Page({
       'selectTypeCounts': selectedTypeCounts,
       'account': account / (multiple * multiple)
     }
+  },
+
+  /**
+  * 单选按钮
+  */
+  toggleSelect:function(event){
+    var id = cart.getDataSet(event, 'id');
+    var status = cart.getDataSet(event, 'status');
+    var index = this._getProductIndexById(id);
+    this.data.cartData[index].selectStatus = !status;
+    this._restCartData();
+  },
+  
+  /**
+   * 多选按钮
+   */
+  toggleSelectAll:function(event){
+
+  },
+
+  /**
+   * 根据id获取商品index下标
+   */
+  _getProductIndexById: function(id){
+    var data = this.data.cartData;
+    var len = data.length;
+    for (let i = 0 ; i < len; i++){
+      if (data[i].id == id){
+        return i;
+      }
+    }
+  },
+
+  /**
+   * 计算选中商品的总金额
+   * 改变选中状态
+   */
+  _restCartData:function(){
+    var newData = this._calcTotalAccountAndCounts(this.data.cartData);
+    this.setData({
+      'account': newData.account,
+      'selectedCounts': newData.selectedCount,
+      'selectedTypeCount': newData.selectedTypeCounts,
+      'cartData': this.data.cartData
+    });
   }
 })
